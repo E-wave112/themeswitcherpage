48fbb67353c537ed24ebab5390a510ff273ef170
E-wave112 <osagieiyayi09@gmail.com>
2021-02-08 13:24:03 +0000

    added the tooggle functionality

--- class.js
+++ class.js
@@ -1,6 +1,6 @@
 // scripts
 const body = document.querySelector('#body');
-const btn = document.querySelector('.button');
+const btn = document.querySelector('.btn-toggle');
 const innercards = document.querySelectorAll('.div');
 const secondFlex = document.querySelector('.second-flex');
 const thirdFlex = document.querySelector('.third-flex');
@@ -10,10 +10,21 @@ const nums2 = document.querySelectorAll('.num2');
 const caps = document.querySelectorAll('.cap');
 const togglers = document.querySelectorAll('.toggls');
 const grayText = document.querySelectorAll('.gray');
+const modeName = document.querySelector('.mode-name');
 console.log(innercards);
 console.log(typeof nums2)
 
 btn.addEventListener('click', function() {
+
+    if (btn.classList.contains("active")) {
+        btn.classList.remove("active");
+        modeName.innerHTML = "Dark Mode"
+        modeName.style.color = "hsl(228, 34%, 66%)"
+      } else {
+        btn.classList.add("active");
+        modeName.innerHTML = "Light Mode"
+        modeName.style.color = "hsl(228, 12%, 44%)"
+      }
     body.classList.toggle('black-bg');
     headings.classList.toggle('color');
     grayText.forEach(function(gray){
